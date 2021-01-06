54cc56a1cb8e6f866a6a6279ecd36ffae491d64c
Prageeth Silva <prageethsilva@gmail.com>
2021-01-06 02:52:40 +0100

    support excluding

--- src/Models/Sitemap.php
+++ src/Models/Sitemap.php
@@ -14,10 +14,21 @@ class Sitemap
     {
         $sitemap = new static;
 
-        return collect()
-            ->merge(self::toSitemapEntries($sitemap->publishedEntries()))
-            ->merge(self::toSitemapEntries($sitemap->publishedTerms()))
-            ->merge(self::toSitemapEntries($sitemap->publishedCollectionTerms()))
+        $entries = collect();
+
+        if (config('pecotamic.sitemap.include_entries')) {
+            $entries = $entries->merge(self::toSitemapEntries($sitemap->publishedEntries()));
+        }
+
+        if (config('pecotamic.sitemap.include_terms')) {
+            $entries = $entries->merge(self::toSitemapEntries($sitemap->publishedTerms()));
+        }
+
+        if (config('pecotamic.sitemap.include_collection_terms')) {
+            $entries = $entries->merge(self::toSitemapEntries($sitemap->publishedCollectionTerms()));
+        }
+
+        return $entries
             ->values()
             ->sortBy(function (SitemapEntry $entry) {
                 return substr_count(rtrim($entry->path, '/'), '/');
@@ -34,12 +45,25 @@ class Sitemap
 
     protected function publishedEntries(): \Illuminate\Support\Collection
     {
+        $exluded_urls = self::getExcludedUrlCollection();
+        $entry_types = config('pecotamic.sitemap.entry_types');
+
         return Collection::all()
             ->flatMap(function ($collection) {
                 return $collection->queryEntries()->get();
             })
-            ->filter(function ($entry) {
+            ->filter(function ($entry) use ($exluded_urls, $entry_types) {
                 if (!preg_match('#^https?://#', $entry->absoluteUrl())) {
+                    return false;
+                }
+
+                // is excluded by url pattern
+                if (self::isExcluded($entry->url(), $exluded_urls)) {
+                    return false;
+                }
+
+                // is an included entry type
+                if ($entry_types !== null && !in_array($entry->collectionHandle(), $entry_types)) {
                     return false;
                 }
 
@@ -49,19 +73,28 @@ class Sitemap
 
     protected function publishedTerms()
     {
+        $exluded_urls = self::getExcludedUrlCollection();
+
         return Taxonomy::all()
             ->flatMap(function ($taxonomy) {
                 return $taxonomy->queryTerms()->get();
             })
             ->filter
             ->published()
-            ->filter(function ($term) {
+            ->filter(function ($term) use ($exluded_urls) {
+
+                if (self::isExcluded($entry->url(), $exluded_urls)) {
+                    return false;
+                }
+
                 return view()->exists($term->template());
             });
     }
 
     protected function publishedCollectionTerms()
     {
+        $exluded_urls = self::getExcludedUrlCollection();
+
         return Collection::all()
             ->flatMap(function ($collection) {
                 return $collection->taxonomies()->map->collection($collection);
@@ -71,8 +104,25 @@ class Sitemap
             })
             ->filter
             ->published()
-            ->filter(function ($term) {
+            ->filter(function ($term) use ($exluded_urls) {
+
+                if (self::isExcluded($entry->url(), $exluded_urls)) {
+                    return false;
+                }
+
                 return view()->exists($term->template());
             });
+    }
+
+    private static function isExcluded($url, $exluded_urls)
+    {
+        return $exluded_urls->contains(function ($value) {
+            return preg_match($value, $url);
+        });
+    }
+
+    private function getExcludedUrlCollection()
+    {
+        return collect(config('pecotamic.sitemap.exclude_urls', []));
     }
 }
