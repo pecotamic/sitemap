211413b6a3aeeb07881b98e77ea476371e2e88d6
Werner Freytag <freytag@gmx.de>
2021-01-07 20:09:00 +0100

    cleanup code

--- src/Models/Sitemap.php
+++ src/Models/Sitemap.php
@@ -15,20 +15,23 @@ class Sitemap
         $sitemap = new static;
 
         $entries = collect();
-
         if (config('pecotamic.sitemap.include_entries', true)) {
-            $entries = $entries->merge(self::toSitemapEntries($sitemap->publishedEntries()));
+            $entries = $entries->merge($sitemap->publishedEntries(config('pecotamic.sitemap.entry_types')));
         }
-
         if (config('pecotamic.sitemap.include_terms', true)) {
-            $entries = $entries->merge(self::toSitemapEntries($sitemap->publishedTerms()));
+            $entries = $entries->merge($sitemap->publishedTerms());
         }
-
         if (config('pecotamic.sitemap.include_collection_terms', true)) {
-            $entries = $entries->merge(self::toSitemapEntries($sitemap->publishedCollectionTerms()));
+            $entries = $entries->merge($sitemap->publishedCollectionTerms());
+        }
+        if ($excludedUrls = config('pecotamic.sitemap.exclude_urls')) {
+            $entries = self::filterEntriesByUrlPatterns($entries, $excludedUrls);
         }
 
         return $entries
+            ->map(function ($entry) {
+                return new SitemapEntry($entry);
+            })
             ->values()
             ->sortBy(function (SitemapEntry $entry) {
                 return substr_count(rtrim($entry->path, '/'), '/');
@@ -36,64 +39,39 @@ class Sitemap
             ->toArray();
     }
 
-    protected static function toSitemapEntries($items)
+    protected function publishedEntries(?array $entryTypes = null): \Illuminate\Support\Collection
     {
-        return $items->map(function ($content) {
-            return new SitemapEntry($content);
-        });
-    }
-
-    protected function publishedEntries(): \Illuminate\Support\Collection
-    {
-        $exluded_urls = $this->getExcludedUrlCollection();
-        $entry_types = config('pecotamic.sitemap.entry_types');
-
         return Collection::all()
             ->flatMap(function ($collection) {
                 return $collection->queryEntries()->get();
             })
-            ->filter(function ($entry) use ($exluded_urls, $entry_types) {
-                if (!preg_match('#^https?://#', $entry->absoluteUrl())) {
+            ->filter(function ($entry) use ($entryTypes) {
+                if (!self::isAbsoluteUrl($entry->absoluteUrl())) {
+                    return false;
+                }
+                if ($entryTypes !== null && !in_array($entry->collectionHandle(), $entryTypes)) {
                     return false;
                 }
 
-                // is excluded by url pattern?
-                if ($this->isExcluded($entry->url(), $exluded_urls)) {
-                    return false;
-                }
-
-                // is an included entry type?
-                if ($entry_types !== null && !in_array($entry->collectionHandle(), $entry_types)) {
-                    return false;
-                }
-
-                return $entry->status() === 'published';
+                return $entry->published();
             });
     }
 
     protected function publishedTerms()
     {
-        $exluded_urls = $this->getExcludedUrlCollection();
-
         return Taxonomy::all()
             ->flatMap(function ($taxonomy) {
                 return $taxonomy->queryTerms()->get();
             })
             ->filter
             ->published()
-            ->filter(function ($term) use ($exluded_urls) {
-                if ($this->isExcluded($term->url(), $exluded_urls)) {
-                    return false;
-                }
-
+            ->filter(function ($term) {
                 return view()->exists($term->template());
             });
     }
 
     protected function publishedCollectionTerms()
     {
-        $exluded_urls = $this->getExcludedUrlCollection();
-
         return Collection::all()
             ->flatMap(function ($collection) {
                 return $collection->taxonomies()->map->collection($collection);
@@ -103,24 +81,27 @@ class Sitemap
             })
             ->filter
             ->published()
-            ->filter(function ($term) use ($exluded_urls) {
-                if ($this->isExcluded($term->url(), $exluded_urls)) {
-                    return false;
-                }
-
+            ->filter(function ($term) {
                 return view()->exists($term->template());
             });
     }
 
-    private function isExcluded($url, $exluded_urls)
+    protected static function isAbsoluteUrl(string $url): bool
     {
-        return $exluded_urls->contains(function ($value) use ($url) {
-            return preg_match($value, $url);
-        });
+        return preg_match('#^https?://#', $url);
     }
 
-    private function getExcludedUrlCollection()
+    protected static function filterEntriesByUrlPatterns(\Illuminate\Support\Collection $entries, array $excludedUrls): \Illuminate\Support\Collection
     {
-        return collect(config('pecotamic.sitemap.exclude_urls', []));
+        return $entries->filter(function ($entry) use ($excludedUrls) {
+            $url = $entry->url();
+            foreach ($excludedUrls as $pattern) {
+                if (preg_match($pattern, $url)) {
+                    return false;
+                }
+            }
+
+            return true;
+        });
     }
 }
